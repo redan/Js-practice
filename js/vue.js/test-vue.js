8f18812d5e842594f458c0405c9ce3aaf9338776
Daniil.Suchkov <rewasors@gmail.com>
2020-06-02 08:59:39 +0300

    Основы vue

--- js/vue.js/test-vue.js
+++ js/vue.js/test-vue.js
@@ -2,5 +2,20 @@ var app = new Vue({
     el: "#app",
     data: {
         message: "Hello vue!",
+        say: "My text!",
+        seen: true,
+    },
+});
+
+var app2 = new Vue({
+    el: "#tonggle-button",
+    data: {
+        tongle: function () {
+            if(app.seen) {
+                app.seen = 0;
+            } else {
+                app.seen = 1;
+            }
+        }
     }
 })
